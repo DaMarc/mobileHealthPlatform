03bfe24529560e52814c1e25bf44a9f2d1e48141
albone <Alban_K_@live.de>
2016-05-25 12:00:44 +0200

    merge conflicts

--- vendor/assets/js/app.js
+++ vendor/assets/js/app.js
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
-var $ = require('jquery')(window);
-global.jQuery = require("jquery");
-require ('../../../node_modules/jquery-mobile/dist/jquery.mobile.js');
-=======
 var $ = require('jquery');
 $.mobile = require ('jquery-mobile');
-
->>>>>>> structure/vendor-functions
